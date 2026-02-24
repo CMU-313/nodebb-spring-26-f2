f080cbd5c6b4d261f289c9754081c9fe0eb851f5
Justin Wu <justinw5@andrew.cmu.edu>
2026-02-24 06:08:42 +0000

    feat(admin/tags): add category-scoped tag management

--- src/socket.io/admin/tags.js
+++ src/socket.io/admin/tags.js
@@ -13,16 +13,26 @@ Tags.create = async function (socket, data) {
 };
 
 Tags.rename = async function (socket, data) {
-	if (!Array.isArray(data)) {
+	if (!Array.isArray(data) && !(data && Array.isArray(data.tags))) {
 		throw new Error('[[error:invalid-data]]');
 	}
 
-	await topics.renameTags(data);
+	if (data && Array.isArray(data.tags) && data.cid) {
+		await topics.renameTagsInCategory(data.tags, data.cid);
+		return;
+	}
+
+	await topics.renameTags(Array.isArray(data) ? data : data.tags);
 };
 
 Tags.deleteTags = async function (socket, data) {
-	if (!data) {
+	if (!data || !Array.isArray(data.tags)) {
 		throw new Error('[[error:invalid-data]]');
+	}
+
+	if (data.cid) {
+		await topics.deleteTagsFromCategory(data.tags, data.cid);
+		return;
 	}
 
 	await topics.deleteTags(data.tags);
