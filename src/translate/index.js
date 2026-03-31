64db3c66299b0e82784b7ec0968f4b4ceef4724f
Cindy Wang <xinyuew4@andrew.cmu.edu>
2026-03-31 18:49:33 -0400

    Fix translator API lint formatting

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,27 +5,27 @@ const translatorApi = module.exports;
 const DEFAULT_TRANSLATOR_API = 'http://127.0.0.1:5000';
 
 translatorApi.translate = async function (postData) {
-    const content = (postData && typeof postData.content === 'string') ? postData.content : '';
-    const translatorApiBase = process.env.TRANSLATOR_API || DEFAULT_TRANSLATOR_API;
+	const content = (postData && typeof postData.content === 'string') ? postData.content : '';
+	const translatorApiBase = process.env.TRANSLATOR_API || DEFAULT_TRANSLATOR_API;
 
-    if (!content) {
-        return [true, content];
-    }
+	if (!content) {
+		return [true, content];
+	}
 
-    try {
-        const url = new URL('/', translatorApiBase);
-        url.searchParams.set('content', content);
+	try {
+		const url = new URL('/', translatorApiBase);
+		url.searchParams.set('content', content);
 
-        const response = await fetch(url.toString());
-        if (!response.ok) {
-            throw new Error(`Translator service returned ${response.status}`);
-        }
+		const response = await fetch(url.toString());
+		if (!response.ok) {
+			throw new Error(`Translator service returned ${response.status}`);
+		}
 
-        const data = await response.json();
-        const isEnglish = typeof data.is_english === 'boolean' ? data.is_english : true;
-        const translatedContent = typeof data.translated_content === 'string' ? data.translated_content : content;
-        return [isEnglish, translatedContent];
-    } catch (err) {
-        return [true, content];
-    }
+		const data = await response.json();
+		const isEnglish = typeof data.is_english === 'boolean' ? data.is_english : true;
+		const translatedContent = typeof data.translated_content === 'string' ? data.translated_content : content;
+		return [isEnglish, translatedContent];
+	} catch (err) {
+		return [true, content];
+	}
 };
