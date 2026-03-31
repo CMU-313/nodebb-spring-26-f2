aea3b0f0264083e8175ab58a71b64c42a95dbe16
Justin Wu <justinw5@andrew.cmu.edu>
2026-03-31 21:27:43 +0000

    asdf

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,14 +4,23 @@
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
-};
+translatorApi.translate = async function (postData) {
+	const TRANSLATOR_API = process.env.TRANSLATOR_API || 'http://127.0.0.1:5000';
 
-// translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
-//  const TRANSLATOR_API = "TODO"
-//  const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-//  const data = await response.json();
-//  return ['is_english','translated_content'];
-// };
+	try {
+		const content = postData && postData.content ? String(postData.content) : '';
+		const url = `${TRANSLATOR_API}/?content=${encodeURIComponent(content)}`;
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			throw new Error(`Translator service returned ${response.status}`);
+		}
+
+		const data = await response.json();
+		const isEnglish = Boolean(data && data.is_english);
+		const translatedContent = (data && typeof data.translated_content === 'string') ? data.translated_content : '';
+		return [isEnglish, translatedContent];
+	} catch (err) {
+		return [true, ''];
+	}
+};
